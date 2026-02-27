9c4be8949c7b32aeaa6ae43dfdeffb5b6e4ebbf5
thekrishnamsingh <thekrishnamsingh@gmail.com>
2026-02-28 00:13:29 +0530

    day03 models.py

---
+++ models3.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, Integer, String, Float
+from app.database import Base
+
+
+class Item(Base):
+    __tablename__ = "items"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    price = Column(Float)
+    description = Column(String, nullable=True)
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
